5d38be2b3814361fefb97580f8b546767ef5b984
Amdad92 <emdadctg92@gmail.com>
2024-12-06 15:47:07 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let btn=document.querySelectorAll(".btn");
+let resetBtn=document.querySelector("#reset-btn");
+let turn0=true; //playerX, playerY
+
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+   
+]
